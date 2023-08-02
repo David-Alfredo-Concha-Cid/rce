a16f263ca040bf2dd63f392f039edb50886eb4a6
david <d.conchacid@gmail.com>
2023-08-01 20:42:52 -0400

    vercel

--- rce/settings.py
+++ rce/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'rce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'concha_rce',
+        'USER': 'concha_ admin_rce',
+        'PASSWORD': 'rcedjango',
+        'HOST': 'postgresql-concha.alwaysdata.net',
     }
 }
+
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 
 # Password validation
@@ -126,3 +138,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
